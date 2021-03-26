defa7250b95d88c3355a5cd649e5f60f151928a1
vinicius <vinicius.faleiros@hotmail.com>
2021-03-26 09:41:43 -0300

    Alteracao no Mirage

--- src/index.tsx
+++ src/index.tsx
@@ -18,15 +18,7 @@ createServer({
           category: 'Dev',
           amount: 6000,
           createdAt: new Date('2021-02-12 9:00:00'),
-        },
-        {
-          id: 2,
-          title: 'Alguel',
-          type: 'withdraw',
-          category: 'Casa',
-          amount: 1100,
-          createdAt: new Date('2021-02-14 11:00:00'),
-        },
+        }
       ],
     });
   },
